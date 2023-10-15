ce9d22510e99e4076637f7d7dbb0f20d2b892669
Alexandra Egorova <egorova.aleksandra@gmail.com>
2023-10-15 11:21:13 +0200

    deleted consoles

--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -51,47 +51,43 @@ export default function Product() {
   const handleClick = (e) => {
     e.preventDefault();
     let fileName = product.img;
-    console.log(fileName)
     if (file !== null) {
       fileName = new Date().getTime() + file.name;
-    
-    
-    const storage = getStorage();
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+      const storage = getStorage();
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on(
-      'state_changed', 
-      (snapshot) => {
-        const progress = 
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
-          default:
+      uploadTask.on(
+        'state_changed', 
+        (snapshot) => {
+          const progress = 
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+            default:
+          }
+        }, 
+        (error) => {
+          // Handle unsuccessful uploads
+        }, 
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            let newproduct = { ...product, img: downloadURL };
+            setProduct(newproduct);
+            updateProduct(productId, newproduct, dispatch);
+          });
         }
-      }, 
-      (error) => {
-        // Handle unsuccessful uploads
-      }, 
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          let newproduct = { ...product, img: downloadURL };
-          setProduct(newproduct)
-          console.log(product)
-          updateProduct(productId, newproduct, dispatch);
-        });
-      }
-    );
-  } else {
-    setProduct(product)
-    updateProduct(productId, product, dispatch);
-  }
+      );
+    } else {
+      setProduct(product);
+      updateProduct(productId, product, dispatch);
+    }
   };
 
   return (
